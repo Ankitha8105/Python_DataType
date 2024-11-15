b25eeb7dbc1b8256e5dee1b01dc8340ce43285ec
Ankitha8105 <ankithabl2001@gmail.com>
2024-11-15 15:38:13 +0530

    [Ankitha] Add: Python program to print the documents (syntax, description etc.) of Python built-in

---
+++ Basic_prgms/4_Docstring.py
@@ -0,0 +1,22 @@
+'''
+    @Author: Ankitha B L
+    @Date:11 -11-2024
+    @Last Modified by: Ankitha B L
+    @Last Modified time: 11 - 11-2024
+    @Title : print the documentation of built-in file problem
+'''
+
+def doc_str():
+    """ 
+    Description :
+        This function is used to print the Documentation of abs function
+    Return :
+        It returns the Documentation of in built funcatio absolute value
+    """
+    print(abs.__doc__)
+
+def main():
+    doc_str()
+
+if __name__ == "__main__":
+    main()
