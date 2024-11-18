8b64f19211ffe1c6da5943f8fac3ae1fc155a2f7
Ankitha8105 <ankithabl2001@gmail.com>
2024-11-18 22:55:38 +0530

    [Ankitha] Add :  Python program to create a set

---
+++ Set_prgms/1_CreateSet.py
@@ -0,0 +1,27 @@
+'''
+    @Author: Ankitha B L
+    @Date:18-11-2024
+    @Last Modified by: Ankitha B L
+    @Last Modified time: 18- 11-2024
+    @Title: Python program to create a set problem
+'''
+
+def create_set(set_ele):
+    """ 
+    Description :
+        This function is used to Python program to create a set
+    Parameters :
+        set_ele = set of items
+    Return :
+        It returns the set items
+    """
+    
+    return set_ele
+
+def main():
+    set_ele = {1,3,4,5,2,1,3,2}
+    res_set = create_set(set_ele)
+    print(f"The set elements are {res_set}")
+
+if __name__ =="__main__":
+    main()
